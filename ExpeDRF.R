0881fe11c232bfe62458d56378c67ece95557ab7
shan-stat <64387506+shan-stat@users.noreply.github.com>
2020-04-28 14:07:19 -0700

    Add files via upload

---
+++ ExpeDRF.R
@@ -0,0 +1,27 @@
+########## DRF example ##########
+### Benchmark experiment using "iris" dataset ###
+library(datasets)
+dat <- iris
+n <- nrow(dat) # total instances
+R <- 100 # number of repetition
+
+
+# Runing step : repeat the experiment 100 times #
+res <- c()
+for(i in 1:R){
+  print(i)
+  
+  # Splitting data into 50% of training set and 50% of test set #
+  set.seed(12345*i)
+  idx <- sample(n, n, replace = F)
+  train.idx <- idx[1:ceiling(n*0.5)] ; test.idx <- idx[-(1:ceiling(n*0.5))]
+  train.dat <- dat[train.idx,] ; test.dat <- dat[test.idx,]
+  
+  # Fitting DRF and Evaluating test accuracy #
+  set.seed(123)
+  fit.drf <- DRF.formula(Species~., data=train.dat, ntree=200)
+  pred.drf <- predict(fit.drf, test.dat)
+  res.drf <- table(pred.drf, test.dat$Species)
+  res[i] <- sum(diag(res.drf))/sum(res.drf)
+}
+res
